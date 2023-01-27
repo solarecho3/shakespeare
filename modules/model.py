e1ca27e8100041f4697f001954e4e7a4306bc3a6
solarecho3 <solarecho3@pm.me>
2023-01-27 07:35:33 -0500

    setup data configs yaml

--- modules/model.py
+++ modules/model.py
@@ -1,6 +1,7 @@
 import os
 import logging
-"""Accesses data, updates the model."""
+import yaml
+from typing import Any
 
 class DataLoader:
 
@@ -14,18 +15,40 @@ class DataLoader:
         path: str | os.PathLike - Relative path to the data folder. Default = ../data/
         """
         
-        self.datapath = os.path.abspath(datapath)
-        print(f"DataLoader initialized with {self.datapath}...")
+        self.configpath = os.path.abspath('../config/')
+        
+        with open(
+            f'{self.configpath}/datasets.yml',
+            'r',
+            encoding='utf-8') as f:
+
+            self.configs = yaml.load(f, Loader=yaml.Loader)
 
     def tiny(self):
         """
         Load the tiny shakespeare data set.
         """
+        
         with open(
-            f"{self.datapath}/tinyshakespeare.txt",
+            self.configs['tiny']['path'],
             'r',
             encoding='utf-8') as f:
 
             text = f.read()
         
         self.data = text
+
+    def complete(self):
+        """
+        Load The Complete Works of William Shakespeare by Gutenberg data set.
+        """
+        with open(
+            self.configs['complete']['path'],
+            'r',
+            encoding='utf-8') as f:
+
+            text = f.read()
+
+        self.data = text
+
+    
